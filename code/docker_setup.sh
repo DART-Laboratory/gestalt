e16a9454efa3d7ab4814d3f38a1d96a03a3aa7c2
Mati Ur Rehman <rehman_m31@yahoo.com>
2024-10-27 14:11:59 -0400

    updated docker script

--- code/docker_setup.sh
+++ code/docker_setup.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-# Check for correct number of arguments
+# Usage: ./script_name.sh <port_number> <user_name>
+# This script starts a Jupyter notebook in a Docker container inside a screen session.
+# Arguments:
+#   <port_number> - The port on which the Jupyter notebook will be accessible.
+#   <user_name> - The username based on which the working directory will be created and named.
+
+# Check for the correct number of arguments
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <port_number> <working_dir>"
+    echo "Usage: $0 <port_number> <user_name>"
     exit 1
 fi
 
 # Extract arguments
 port_number=$1
-working_dir=$2
+user_name=$2
+working_dir="${user_name}_dir"
 
 # Check if the specified directory exists, create if it doesn't
 if [ ! -d "$working_dir" ]; then
@@ -20,8 +27,10 @@ if [ ! -d "$working_dir" ]; then
     fi
 fi
 
-# Run Docker command
-docker run -it -p ${port_number}:8888 --user root -e GRANT_SUDO=yes --group-add users \
+# Use screen to run Docker in a new session named after the user
+screen -dmS $user_name bash -c "docker run -it -p ${port_number}:8888 --user root -e GRANT_SUDO=yes --group-add users \
 -v /bigdisk/Trustwatch:/home/jovyan/Trustwatch:ro \
--v ${working_dir}:/home/jovyan/${working_dir} \
-jupyter/base-notebook start.sh jupyter notebook --NotebookApp.token=''
+-v ${PWD}/${working_dir}:/home/jovyan/${working_dir} \
+jupyter/base-notebook start.sh jupyter notebook --NotebookApp.token=''"
+
+echo "Started Jupyter Notebook in Docker container within screen session named $user_name"
