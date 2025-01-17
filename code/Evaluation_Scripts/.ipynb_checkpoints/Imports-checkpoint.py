c56402d193a3d6368f492d31cb0532cac727bc48
Aaron4253 <aaronli715@gmail.com>
2025-01-17 02:48:18 +0000

    simplifed import file

--- code/Evaluation_Scripts/.ipynb_checkpoints/Imports-checkpoint.py
+++ code/Evaluation_Scripts/.ipynb_checkpoints/Imports-checkpoint.py
@@ -1,4 +1,3 @@
-#Import for Darpa_E3
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
@@ -10,62 +9,24 @@ import json
 import warnings
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
-warnings.filterwarnings('ignore')
 from torch_geometric.loader import NeighborLoader
 import multiprocessing
 from pprint import pprint
 import gzip
-from sklearn.manifold import TSNE
-import json
-import copy
-import os
-from torch_geometric.nn import GCNConv
-from torch_geometric.nn import SAGEConv, GATConv
-import torch.nn.functional as F
+from torch_geometric.nn import GCNConv, SAGEConv, GATConv
 import torch.nn as nn
 from gensim.models.callbacks import CallbackAny2Vec
-import gensim
 from gensim.models import Word2Vec
-from multiprocessing import Pool
 from itertools import compress
 from tqdm import tqdm
 import time
 from sklearn.utils import class_weight
-import torch.nn.functional as F
 from torch.nn import CrossEntropyLoss
-from torch.nn import CrossEntropyLoss
-from sklearn.utils import class_weight
-import copy
-from multiprocessing import Pool
-import time
-import pandas as pd
-from itertools import compress
-from torch_geometric import utils
-#Import for Darpa_E5_CADETS
-from pprint import pprint
-import gzip
-from sklearn.manifold import TSNE
-import json
-import copy
-import os
-from torch_geometric.nn import GCNConv
-from torch_geometric.nn import SAGEConv, GATConv
-import torch.nn.functional as F
-import torch.nn as nn
-from gensim.models.callbacks import CallbackAny2Vec
-import gensim
-from gensim.models import Word2Vec
-from multiprocessing import Pool
-from itertools import compress
-from tqdm import tqdm
-import time
-from sklearn.utils import class_weight
-import torch.nn.functional as F
-from torch.nn import CrossEntropyLoss
-from torch.nn import CrossEntropyLoss
-from sklearn.utils import class_weight
-import copy
-import random
-from itertools import compress
 from torch_geometric import utils
 from collections import Counter
+from elasticsearch import Elasticsearch, helpers
+import re
+import concurrent.futures
+
+# Suppress warnings
+warnings.filterwarnings('ignore')
--- code/Evaluation_Scripts/Imports.py
+++ code/Evaluation_Scripts/Imports.py
@@ -1,4 +1,3 @@
-#Import for Darpa_E3 ----------------------------------------------------------------------------------------------------------------------------
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
@@ -10,150 +9,24 @@ import json
 import warnings
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
-warnings.filterwarnings('ignore')
 from torch_geometric.loader import NeighborLoader
 import multiprocessing
 from pprint import pprint
 import gzip
-from sklearn.manifold import TSNE
-import json
-import copy
-import os
-from torch_geometric.nn import GCNConv
-from torch_geometric.nn import SAGEConv, GATConv
-import torch.nn.functional as F
+from torch_geometric.nn import GCNConv, SAGEConv, GATConv
 import torch.nn as nn
 from gensim.models.callbacks import CallbackAny2Vec
-import gensim
 from gensim.models import Word2Vec
-from multiprocessing import Pool
 from itertools import compress
 from tqdm import tqdm
 import time
 from sklearn.utils import class_weight
-import torch.nn.functional as F
 from torch.nn import CrossEntropyLoss
-from torch.nn import CrossEntropyLoss
-from sklearn.utils import class_weight
-import copy
-from multiprocessing import Pool
-import time
-import pandas as pd
-from itertools import compress
-from torch_geometric import utils
-#Import for Darpa_E5_CADETS ------------------------------------------------------------------------------------------------------------------
-from pprint import pprint
-import gzip
-from sklearn.manifold import TSNE
-import json
-import copy
-import os
-from torch_geometric.nn import GCNConv
-from torch_geometric.nn import SAGEConv, GATConv
-import torch.nn.functional as F
-import torch.nn as nn
-from gensim.models.callbacks import CallbackAny2Vec
-import gensim
-from gensim.models import Word2Vec
-from multiprocessing import Pool
-from itertools import compress
-from tqdm import tqdm
-import time
-from sklearn.utils import class_weight
-import torch.nn.functional as F
-from torch.nn import CrossEntropyLoss
-from torch.nn import CrossEntropyLoss
-from sklearn.utils import class_weight
-import copy
-import random
-from itertools import compress
 from torch_geometric import utils
 from collections import Counter
-#Imports for Darpa_E5_Clearscope ----------------------------------------------------------------------------------------------
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import LabelEncoder
-import torch
-from torch_geometric.data import Data
-import os
-import torch.nn.functional as F
-import json 
-import warnings
-import matplotlib.pyplot as plt
-from sklearn.manifold import TSNE
-warnings.filterwarnings('ignore')
-from torch_geometric.loader import NeighborLoader
-import multiprocessing
 from elasticsearch import Elasticsearch, helpers
 import re
-from tqdm import tqdm
-from pprint import pprint
-import gzip
-from sklearn.manifold import TSNE
-import json
-import copy
-import os
-from itertools import compress
-from torch_geometric import utils
-from torch.nn import CrossEntropyLoss
-from sklearn.utils import class_weight
-import copy
-from collections import Counter
-from sklearn.utils import class_weight
-import torch.nn.functional as F
-from torch.nn import CrossEntropyLoss
-from gensim.models.callbacks import CallbackAny2Vec
-import gensim
-from gensim.models import Word2Vec
-from multiprocessing import Pool
-from itertools import compress
-from tqdm import tqdm
-import time
-from torch_geometric.nn import GCNConv
-from torch_geometric.nn import SAGEConv, GATConv
-import torch.nn.functional as F
-import torch.nn as nn
 import concurrent.futures
-#Imports for  DARPA_E5_THEIA --------------------------------------------------------------------------------------------------------------
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import LabelEncoder
-import torch
-from torch_geometric.data import Data
-import os
-import torch.nn.functional as F
-import json 
-import warnings
-import matplotlib.pyplot as plt
-from sklearn.manifold import TSNE
+
+# Suppress warnings
 warnings.filterwarnings('ignore')
-from torch_geometric.loader import NeighborLoader
-import multiprocessing
-from elasticsearch import Elasticsearch, helpers
-import re
-from tqdm import tqdm
-from pprint import pprint
-import gzip
-from sklearn.manifold import TSNE
-import json
-import copy
-import os
-from torch_geometric.nn import GCNConv
-from torch_geometric.nn import SAGEConv, GATConv
-import torch.nn.functional as F
-import torch.nn as nn
-from gensim.models.callbacks import CallbackAny2Vec
-import gensim
-from gensim.models import Word2Vec
-from multiprocessing import Pool
-from itertools import compress
-from tqdm import tqdm
-import time
-from sklearn.utils import class_weight
-import torch.nn.functional as F
-from torch.nn import CrossEntropyLoss
-from torch.nn import CrossEntropyLoss
-from sklearn.utils import class_weight
-import copy
-from itertools import compress
-from torch_geometric import utils
