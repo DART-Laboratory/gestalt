3f36b732d042d996d7355c2b6ddc09ade5821eda
Aaron4253 <aaronli715@gmail.com>
2025-02-28 06:10:28 +0000

    ran E5 Cadets added output for cross reference

---
+++ startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install git 
+git init
+pip install pandas numpy scikit-learn torch torch-geometric matplotlib gensim scikit-learn elasticsearch pyarrow fastparquet
+git config --global user.name "Aaron4253"
+git config --global user.name
+git config user.email "aaronli715@gmail.com"
+git config user.email
