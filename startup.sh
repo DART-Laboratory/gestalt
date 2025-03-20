6eaf81f3befee211109a3f27c5d75a2624837d30
Aaron4253 <aaronli715@gmail.com>
2025-03-20 23:49:02 +0000

    added timing to other long functions

--- startup.sh
+++ startup.sh
@@ -2,8 +2,8 @@
 sudo apt-get update
 sudo apt-get install git 
 git init
-pip install pandas numpy scikit-learn torch torch-geometric matplotlib gensim scikit-learn elasticsearch pyarrow fastparquet
+pip install pandas numpy scikit-learn torch torch-geometric matplotlib gensim scikit-learn elasticsearch pyarrow fastparquet dask
 git config --global user.name "Aaron4253"
 git config --global user.name
-git config user.email "aaronli715@gmail.com"
-git config user.email
+git config --global user.email "aaronli715@gmail.com"
+git config --global user.email
