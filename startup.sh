0895566b4373aa7fa20defff0be331c2245e0a6a
Aaron4253 <88513672+Aaron4253@users.noreply.github.com>
2025-04-10 21:54:58 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo apt-get install git 
 git init
-pip install pandas numpy scikit-learn torch torch-geometric matplotlib gensim scikit-learn elasticsearch pyarrow fastparquet dask
+pip install pandas numpy scikit-learn torch torch-geometric matplotlib gensim scikit-learn elasticsearch pyarrow fastparquet dask ray
 git config --global user.name "Aaron4253"
 git config --global user.name
 git config --global user.email "aaronli715@gmail.com"
